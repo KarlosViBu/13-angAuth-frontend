fbe783e07e0c340630238a0e3a742cb3c1afa9eb
KarlosViBu <carlosurielvillegas@gmail.com>
2023-08-19 08:11:31 -0400

    for production

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-  baseUrl: 'https://nest-angbackend.up.railway.app/'
+  baseUrl: 'https://nest-angbackend.up.railway.app'
 }
 // baseUrl: 'http://localhost:3000'
 // baseUrl: 'https://nest-auth-backend.up.railway.app'
